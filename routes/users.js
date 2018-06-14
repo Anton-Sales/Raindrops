316dd8ddc5d95e567d2502db33908a4c131f81f2
student <dorpetz@hotmail.com>
2018-06-14 18:07:18 +1200

    Get AJAX post to work

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,19 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('index', {users: users})
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  res.render('index')
+  // db.getUsers()
+  //   .then(users => {
+  //     res.render('index', {users: users})
+  //   })
+  //   .catch(err => {
+  //     res.status(500).send('DATABASE ERROR: ' + err.message)
+  //   })
+})
+
+router.post('/test', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
 })
 
 module.exports = router
